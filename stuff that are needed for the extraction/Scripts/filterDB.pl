340d0afa4daec9825ad7b702a1a2d2ecf09bf6a1
panagiotis <panos@slavelaptop.(none)>
2014-05-22 13:34:29 +0200

    asdf

--- stuff that are needed for the extraction/Scripts/filterDB.pl
+++ stuff that are needed for the extraction/Scripts/filterDB.pl
@@ -3,21 +3,19 @@ use strict;
 use warnings;
 use File::Slurp;
 
-my $file = "../irtm entities/locations.csv";
+my $file = "../Entities\ Dictionary/persons.csv";
 
 my $forGephi = "true";
 
 
 my $text = read_file( $file ) ;
-my $text = $text . read_file("locations.csv");
-my $text = $text . read_file("organization.csv");
+my $text = $text . read_file("../Entities\ Dictionary/locations.csv");
+my $text = $text . read_file("../Entities\ Dictionary/organization.csv");
 $text  =~ s/["\d,]//g;
 my @dictionary = split("\n" ,$text);
 splice(@dictionary, 0, 1);
-$file='DBKnime.csv';
-open(INFO, $file) or die("Could not open  file.");
 
-my $dir = "DBKnimeSplit";
+my $dir = "../DBKnimeSplitByRelation";
 foreach my $fp (glob("$dir/*.csv")) {
 #printf "%s\n", $fp;
 		open my $fh, "<", $fp or die "can't read open ";
@@ -53,16 +51,16 @@ sub filterRelationsOverDictionary{
 																if( index( lc $secondIndividualToken , lc $secondRelation)>-1 && length($secondIndividualToken)== length($secondRelation)){
 																		if($forGephi eq "true"){
 																				my $newRelation ="";
-																				$newRelation = $newRelation . $_ . ';' . $gephiFile . "\n";
-																				$newRelation = $newRelation.  $gephiFile. ';' . $secondRelation  ."\n";
-																				open(FILEZ,  ">>DBKnimeSplit/${gephiFile}gephi.csv");
+																				$newRelation = $newRelation . $_ . ';' . $secondRelation  ."\n";
+																				open(FILEZ,  ">>../DBKnimeSplitInGephiFormat/${gephiFile}.csv");
 																				print FILEZ $newRelation;
+																				#print $newRelation;
 																				close FILEZ;
 																				$matchfound= "true";
 																				last;
 																		}else{
 																				print $line;
-																				open(my $line, '>>', "DBKnimeSplit/${gephiFile}DB.csv");
+																				open(my $line, '>>', "DBKnimeSplitInGephiFormat/${gephiFile}DB.csv");
 																				last
 																		}
 																}
@@ -91,15 +89,15 @@ sub filterRelationsOverDictionary{
 																if( index( lc $secondIndividualToken , lc $secondRelation)>-1  && length($secondIndividualToken)== length($secondRelation)){
 																		if($forGephi eq "true"){
 																				my $newRelation ="";
-																				$newRelation = $newRelation . $secondRelation . ';' . $gephiFile. "\n";
-																				$newRelation = $newRelation .  $gephiFile . ';' . $_ ."\n";
-																				open(FILEZ, ">>DBKnimeSplit/${gephiFile}gephi.csv");
+																				$newRelation = $newRelation . $secondRelation .  ';' . $_ ."\n";
+																				open(FILEZ, ">>../DBKnimeSplitInGephiFormat/${gephiFile}.csv");
 																				print FILEZ $newRelation;
+																				#print $newRelation;
 																				close FILEZ;
 																				$matchfound = "true";
 																				last;
 																		}else{
-																				open(my $line, '>>', "DBKnimeSplit/${gephiFile}DB.csv");
+																				open(my $line, '>>', "DBKnimeSplitInGephiFormat/${gephiFile}DB.csv");
 																				print $line;
 																				last
 																		}
