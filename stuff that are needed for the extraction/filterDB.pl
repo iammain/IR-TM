d02963c71951aa1c3a86edb68c714c7c1e2894f3
panagiotis <panos@slavelaptop.(none)>
2014-05-22 12:55:24 +0200

    fasd

--- stuff that are needed for the extraction/filterDB.pl
+++ stuff that are needed for the extraction/filterDB.pl
@@ -9,91 +9,116 @@ my $forGephi = "true";
 
 
 my $text = read_file( $file ) ;
-
+my $text = $text . read_file("locations.csv");
+my $text = $text . read_file("organization.csv");
 $text  =~ s/["\d,]//g;
 my @dictionary = split("\n" ,$text);
 splice(@dictionary, 0, 1);
 $file='DBKnime.csv';
 open(INFO, $file) or die("Could not open  file.");
-my $counter = 0;
-foreach my $line (<INFO>)  {
-		$counter++;
-		for(@dictionary){
-				my @nouns = split(";", $line);
-				if ( index( lc @nouns[0] , lc $_) > -1){
-						my @individualTokens = split("_" , @nouns[0]);
-						my $matchfound = "false";
-						for my $individualToken(@individualTokens){
-								if ( index( lc $individualToken , lc $_) > -1){
-										for my $secondRelation(@dictionary){
-												my @secondIndividualToken = split("_" , @nouns[2]);
-												for my $secondIndividualToken(@secondIndividualToken){
-														if( index( lc $secondIndividualToken , lc $secondRelation)>-1){
-																if($forGephi eq "true"){
-																		my $newRelation ="";
-																		$newRelation = $newRelation . $_ . ';' . @nouns[1] . "\n";
-																		$newRelation = $newRelation.  @nouns[1] . ';' . $secondRelation  ."\n";
-																		print $newRelation;
-																		$matchfound = "true";
-																		last;
-																}else{
-																		print $line;
-																		last
-																}
-														}
 
+my $dir = "DBKnimeSplit";
+foreach my $fp (glob("$dir/*.csv")) {
+#printf "%s\n", $fp;
+		open my $fh, "<", $fp or die "can't read open ";
+		filterRelationsOverDictionary($fp);
+
+}
+
+
+
+exit(0);
+
+sub filterRelationsOverDictionary{
+		my $file = shift;
+		open(INFO, $file) or die("Could not open  file.");
+
+		my $gephiFile = $file;
+		my @File = split('/' ,$gephiFile);
+		$gephiFile = @File[1];
+		$gephiFile =~ s/\.csv//g;
+
+		foreach my $line (<INFO>)  {
+				print $line;
+				for(@dictionary){
+						my @nouns = split(";", $line);
+						if ( index( lc @nouns[0] , lc $_) > -1){
+								my @individualTokens = split("_" , @nouns[0]);
+								my $matchfound = "false";
+								for my $individualToken(@individualTokens){
+										if ( index( lc $individualToken , lc $_) > -1){
+												for my $secondRelation(@dictionary){
+														my @secondIndividualToken = split("_" , @nouns[1]);
+														for my $secondIndividualToken(@secondIndividualToken){
+																if( index( lc $secondIndividualToken , lc $secondRelation)>-1 && length($secondIndividualToken)== length($secondRelation)){
+																		if($forGephi eq "true"){
+																				my $newRelation ="";
+																				$newRelation = $newRelation . $_ . ';' . $gephiFile . "\n";
+																				$newRelation = $newRelation.  $gephiFile. ';' . $secondRelation  ."\n";
+																				open(FILEZ,  ">>DBKnimeSplit/${gephiFile}gephi.csv");
+																				print FILEZ $newRelation;
+																				close FILEZ;
+																				$matchfound= "true";
+																				last;
+																		}else{
+																				print $line;
+																				open(my $line, '>>', "DBKnimeSplit/${gephiFile}DB.csv");
+																				last
+																		}
+																}
+
+														}
+														if($matchfound eq "true"){
+																last;
+														}
 												}
 												if($matchfound eq "true"){
-													last;
+														last;
 												}
-										}
-									if($matchfound eq "true"){
-										last;
-									}
 
+										}
 								}
-						}
 
 
-				}elsif(index( lc @nouns[2],lc $_) > -1){
-						my @individualTokens = split("_" , @nouns[2]);
-						my $matchfound = "false";
-						for my $individualToken(@individualTokens){
-								if ( index( lc $individualToken , lc $_) > -1){
-										for my $secondRelation(@dictionary){
-												my @secondIndividualToken = split("_" , @nouns[0]);
-												for my $secondIndividualToken(@secondIndividualToken){
-														if( index( lc $secondIndividualToken , lc $secondRelation)>-1){
-																if($forGephi eq "true"){
-																		my $newRelation ="";
-																		$newRelation = $newRelation . $secondRelation . ';' . @nouns[1] . "\n";
-																		$newRelation = $newRelation .  @nouns[1] . ';' . $_ ."\n";
-																		print $newRelation;
-																		$matchfound = "true";
-																		last;
-																}else{
-																		print $line;
-																		last
+						}elsif(index( lc @nouns[1],lc $_) > -1){
+								my @individualTokens = split("_" , @nouns[1]);
+								my $matchfound = "false";
+								for my $individualToken(@individualTokens){
+										if ( index( lc $individualToken , lc $_) > -1){
+												for my $secondRelation(@dictionary){
+														my @secondIndividualToken = split("_" , @nouns[0]);
+														for my $secondIndividualToken(@secondIndividualToken){
+																if( index( lc $secondIndividualToken , lc $secondRelation)>-1  && length($secondIndividualToken)== length($secondRelation)){
+																		if($forGephi eq "true"){
+																				my $newRelation ="";
+																				$newRelation = $newRelation . $secondRelation . ';' . $gephiFile. "\n";
+																				$newRelation = $newRelation .  $gephiFile . ';' . $_ ."\n";
+																				open(FILEZ, ">>DBKnimeSplit/${gephiFile}gephi.csv");
+																				print FILEZ $newRelation;
+																				close FILEZ;
+																				$matchfound = "true";
+																				last;
+																		}else{
+																				open(my $line, '>>', "DBKnimeSplit/${gephiFile}DB.csv");
+																				print $line;
+																				last
+																		}
 																}
+														}
+														if($matchfound eq "true"){
+																last;
 														}
 												}
 												if($matchfound eq "true"){
-													last;
+														last;
 												}
-										}
-										if($matchfound eq "true"){
-											last;
-										}
 
+										}
 								}
 						}
 				}
 		}
-		if($counter>15000){
-				last;
-		}
+		close(INFO);
+
 }
-close(INFO);
-
-
 #print $text;
